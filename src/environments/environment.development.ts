961cfbcbf1422cc5eda12aa07890ee08bbe9c10f
Gabriel-Saint <gabrielsantosayres@gmail.com>
2026-07-08 22:01:18 -0300

    feat: env public do supabase

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  supabaseUrl: 'https://ffwoyjvxyeogicuscvuo.supabase.co',
+  supabaseKey: 'sb_publishable_oO-r6TDNwGh_QmIipRNp0A_o5OGdJIL'
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  supabaseUrl: 'https://ffwoyjvxyeogicuscvuo.supabase.co',
+  supabaseKey: 'sb_publishable_oO-r6TDNwGh_QmIipRNp0A_o5OGdJIL'
+}
