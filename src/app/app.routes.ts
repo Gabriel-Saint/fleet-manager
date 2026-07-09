ad8106ee049e725c5335dd9dc622bba32b8418cd
Gabriel-Saint <gabrielsantosayres@gmail.com>
2026-07-08 23:17:13 -0300

    feat: app routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,33 @@
-import { Routes } from '@angular/router';
+import { Routes } from '@angular/router'
+import { authGuard } from './guards/auth-guard'
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'login',
+    loadComponent: () => import('./components/login/login')
+      .then(c => c.Login)
+  },
+  {
+    path: 'veiculos',
+    canActivate: [authGuard],
+    loadComponent: () => import('./components/veiculo-list/veiculo-list')
+      .then(c => c.VeiculoList)
+  },
+  {
+    path: 'veiculos/novo',
+    canActivate: [authGuard],
+    loadComponent: () => import('./components/veiculo-form/veiculo-form')
+      .then(c => c.VeiculoForm)
+  },
+  {
+    path: 'veiculos/:id/editar',
+    canActivate: [authGuard],
+    loadComponent: () => import('./components/veiculo-form/veiculo-form')
+      .then(c => c.VeiculoForm)
+  },
+  {
+    path: '',
+    redirectTo: 'veiculos',
+    pathMatch: 'full'
+  }
+]
