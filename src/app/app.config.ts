868ee65b83142443dec024a8a2c813813a4a55d9
Gabriel-Saint <gabrielsantosayres@gmail.com>
2026-07-08 22:43:24 -0300

    feat: registrando os interceptors

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,20 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core'
+import { provideRouter } from '@angular/router'
+import { provideHttpClient, withInterceptors } from '@angular/common/http'
+import { provideAnimations } from '@angular/platform-browser/animations'
 
-import { routes } from './app.routes';
+import { routes } from './app.routes'
+import { authInterceptor } from './interceptors/auth-interceptor'
+import { errorInterceptor } from './interceptors/error-interceptor'
+import { loadingInterceptor } from './interceptors/loading-interceptor'
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideAnimations(), // TODO: deprecated na v20.2, mas Angular Material ainda depende disso internamente. Trocar quando o Material migrar pro animate.enter/leave (antes da v23).
+    provideHttpClient(
+      withInterceptors([authInterceptor, loadingInterceptor, errorInterceptor])
+    )
   ]
-};
+}
