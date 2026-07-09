19f1b0033e9f608875b7b170c9bc20dbd9f478ed
Gabriel-Saint <gabrielsantosayres@gmail.com>
2026-07-08 22:00:29 -0300

    feat: criando as models do sistema

---
+++ src/app/models/veiculo.ts
@@ -0,0 +1,11 @@
+export interface Veiculo {
+  id?: string
+  placa: string
+  chassi: string
+  renavam: string
+  modelo: string
+  marca: string
+  ano: number
+  user_id?: string
+  created_at?: string
+}
